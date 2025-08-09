ccf7315134b7820712c425a61f91004d6e80a027
AnthonyClaran <157488965+AnthonyClaran@users.noreply.github.com>
2025-08-09 20:17:38 +0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -2,11 +2,24 @@
 
 use App\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+$autoloadPath = dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+// Debugging block
+if (!file_exists($autoloadPath)) {
+    header('Content-Type: text/plain');
+    echo "CRITICAL: autoload_runtime.php missing!\n\n";
+    echo "Scanned path: $autoloadPath\n\n";
+    
+    echo "Vendor directory contents:\n";
+    echo implode("\n", scandir(dirname($autoloadPath)));
+    
+    echo "\n\nComposer installed packages:\n";
+    passthru('composer show -i');
+    exit(1);
+}
+
+require_once $autoloadPath;
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
-
-echo "Autoload path: " . realpath(__DIR__.'/../vendor/autoload_runtime.php');
-exit();
