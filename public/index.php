609f5793c6fa96bd9d33a1195ad9f6b8276ca5ca
AnthonyClaran <157488965+AnthonyClaran@users.noreply.github.com>
2025-08-16 22:16:02 +0300

    dd

--- public/index.php
+++ public/index.php
@@ -1,19 +1,21 @@
 <?php
 
 use App\Kernel;
-use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
-// Charger .env seulement si présent (utile en dev/local)
-if (file_exists(dirname(__DIR__).'/.env')) {
-    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+$env = $_SERVER['APP_ENV'] ?? 'prod';
+$debug = (bool) ($_SERVER['APP_DEBUG'] ?? false);
+
+if ($debug) {
+    umask(0000);
+    Debug::enable();
 }
 
-// Valeurs par défaut si APP_ENV / APP_DEBUG ne sont pas définis
-$env = $_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? 'prod';
-$debug = (bool) ($_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? false);
-
 $kernel = new Kernel($env, $debug);
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
